c30d6987743dcfd779274c8784e722d78b17c5f8
Gyan Patel <227213233+gyanggw@users.noreply.github.com>
2026-07-02 22:57:19 +0530

    Time: 3 ms (32.23%), Space: 11.8 MB (17.22%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string>key;
+       for (int i = 1; i <= n; ++i) {
+            if (i % 3 == 0 && i%5==0)
+                key.push_back("FizzBuzz");
+            else if (i % 3 == 0)
+                key.push_back("Fizz");
+            else if(i%5==0)
+                key.push_back("Buzz");
+            else
+                key.push_back(to_string(i));
+        }
+        return key;
+    }
+};
